c356b4c517af583401b47267999c63a4856ef46e
Cristino <cristino.lamadrid@xceldata.com>
2017-10-31 15:00:56 -0700

    Added Tino/ to jose branch. Updated the build settings.

---
+++ Assets/Tino/Game/Scripts/Bullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour {
+
+    private Rigidbody2D Body;
+    public Vector2 Direction = new Vector2(1, 0);
+    public Vector2 Speed = new Vector2(1, 1);
+    public int Damage = 1;
+    public bool IsEnemyHit = false;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	}
+
+    void FixedUpdate()
+    {
+        if(this.Body == null) { this.Body = this.GetComponent<Rigidbody2D>(); }
+        this.Body.velocity = new Vector2(this.Direction.x * this.Speed.x, this.Direction.y * this.Speed.y);
+    }
+}
