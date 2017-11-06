2411bd4e3c0cd31a27cab02d49e9888e3799442c
Jose Reyes <jBASSr@users.noreply.github.com>
2017-11-06 15:49:34 -0800

    Add "Boy" player and basic walk cycle

--- Assets/Scripts/Jose/SimpleMovement.cs
+++ Assets/Scripts/Jose/SimpleMovement.cs
@@ -9,10 +9,11 @@ public class SimpleMovement : MonoBehaviour
 	public bool forward = true;
 	public float speedX;
 	public float speedY;
+	public float thrust;
 	private float speed;
 
 	// Jumping
-	public int maxJumps = 5;
+	public int extraJumps = 5;
 	public int jumpCount = 0;
 	public float jumpSpeed = 70;
 
@@ -51,6 +52,7 @@ public class SimpleMovement : MonoBehaviour
 	{
 		//BodyState ();
 		speed = Mathf.Abs(speedX);
+		speedY = rb.velocity.y;
 		anim.SetFloat ("moveX", speedX);
 		anim.SetFloat ("moveY", speedY);
 		anim.SetFloat ("Speed", speed);
@@ -58,9 +60,9 @@ public class SimpleMovement : MonoBehaviour
 		isGrounded = Physics2D.OverlapCircle(groundCheck.position,
 											 groundRadius,
 					 						 whatIsGround);
+		anim.SetBool ("isGrounded", isGrounded);
 		if (isGrounded) {
 			jumpCount = 0;
-			anim.SetBool ("isGrounded", true);
 		}
 		if ((speedX > 0.0f && !forward) || (speedX < 0.0f && forward)) {
 			Flip ();
@@ -82,7 +84,7 @@ public class SimpleMovement : MonoBehaviour
 		}
 		*/
 		speedX = Input.GetKey (KeyCode.A) ? -1 : Input.GetKey (KeyCode.D) ? 1 : 0;
-		speedY = Input.GetKey (KeyCode.S) ? -1 : Input.GetKey (KeyCode.W) ? 1 : 0;
+		// speedY = Input.GetKey (KeyCode.S) ? -1 : Input.GetKey (KeyCode.W) ? 1 : 0;
 		Jump ();
 		if (turning) {
 			turnTime += Time.deltaTime;
@@ -95,16 +97,15 @@ public class SimpleMovement : MonoBehaviour
 
 	void Jump()
 	{
-		anim.SetBool ("isGrounded", false);
 		if (rb.velocity.y >= 0.5f)
 			isMovingUp = true;
 		else
 			isMovingUp = false;
 
 		if (Input.GetKeyDown (KeyCode.Space)) {
-			if ((isGrounded || jumpCount < maxJumps)) {
+			if ((isGrounded || jumpCount < extraJumps)) {
 				rb.AddForce (new Vector2 (0.0f, jumpSpeed));
-				if (jumpCount < maxJumps && !isGrounded)
+				if (jumpCount < extraJumps && !isGrounded)
 					jumpCount++;
 			}
 		}
@@ -125,18 +126,18 @@ public class SimpleMovement : MonoBehaviour
 			transform.localScale = new Vector2(-transform.localScale.x, transform.localScale.y);
 		*/
 	}
-	/*
+
 	void OnCollisionEnter2D (Collision2D c)
 	{
 		if (c.gameObject.tag == "Enemy") {
 			// rigidBody.AddForce(Vector3.left * 100);
 			//rb.AddForce(transform.right * -thrust);
-			rb.AddForce (transform.up * thrust);
-			rb.velocity = new Vector2 (knockback, rb.velocity.y);
+			rb.AddForce (transform.up * (thrust));
+			rb.AddForce (transform.right * -(thrust));
+			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y);
 			// isGrounded = false;
 			// velocity.y += knockbackY;
 			// velocity.x += knockbackX;
 		}
 	}
-	*/
 }
