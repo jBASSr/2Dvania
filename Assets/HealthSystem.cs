b1b38f8dfe0641042ebed76fac1c8fad03c98a19
Jose Reyes <jBASSr@users.noreply.github.com>
2018-02-21 14:37:22 -0800

    begin hp system

---
+++ Assets/HealthSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthSystem : MonoBehaviour {
+	// Health/Armor
+	public int health = 100;
+	//public int armor = 250;
+	// Refs
+	SimpleMovement charSpeed;
+	// Animations
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		// health = Mathf.Clamp(health - 15, 0, 100);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
