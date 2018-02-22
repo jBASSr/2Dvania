90a908b24068e008a3ff8796f78d3ae49124b57f
jBASSr <jrbass00@gmail.com>
2018-02-22 12:05:29 -0800

    Updates to healthsystem

--- Assets/HealthSystem.cs
+++ Assets/HealthSystem.cs
@@ -1,14 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HealthSystem : MonoBehaviour {
 	// Health/Armor
+	public Text healthText;
 	public int health = 100;
 	//public int armor = 250;
 	// Refs
 	SimpleMovement charSpeed;
-	Rigidbody rb;
+	Rigidbody2D rb;
+	CapsuleCollider2D collider;
+	//Text healthText;
 	// Animations
 	SpriteRenderer spriteRenderer;
 	Animator anim;
@@ -19,24 +23,30 @@ public class HealthSystem : MonoBehaviour {
 	void Awake() {
 		charSpeed = GetComponent<SimpleMovement> ();
 		anim = GetComponentInChildren<Animator> ();
-		rb = GetComponent<Rigidbody> ();
+		rb = GetComponent<Rigidbody2D> ();
+		collider = GetComponent<CapsuleCollider2D> ();
+		//healthText = GetComponentInChildren<Text>();
 		spriteRenderer = GetComponentInChildren<SpriteRenderer> ();
 	}
 	// Use this for initialization
 	void Start () {
+		healthText = GetComponent<Text> ();
 		// Init HUD
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		//healthText.text = health.ToString ();
 		// Implement
 		// 1. Invulnerability Timer
 		// 2. Sprite / Animations
 	}
 
 	// Collision with Enemy Hitbox
-	void OnTriggerStay(Collider2D enemy) {
-		if (enemy.tag == "Enemy" && health > 0) {
+	void OnCollisionEnter2D (Collision2D c)
+	{
+		//Debug.Log("Test");
+		if (c.gameObject.tag == "Enemy" && health > 0) {
 			// Deal damage to player
 			health = Mathf.Clamp(health - 15, 0, 100);
 			Debug.Log ("-15 HP!");
@@ -44,14 +54,17 @@ public class HealthSystem : MonoBehaviour {
 			// 1. Update HUD
 			// 2. Death function
 			if (health <= 0) {
+				Debug.Log("Player Died");
 				Death ();
 				return;
 			}
 			// 3. Animations
 			// 4. Invul or Knockback?
-			Vector2 knockback = new Vector2 (charSpeed.forward ? -kbForceX : kbForceX,
-				                    kbForceY);
-			rb.AddForce (knockback);
+			Vector2 knockback = new Vector2 (charSpeed.forward ? -kbForceX : kbForceX, kbForceY);
+			//rb.AddForce (knockback);
+			rb.AddForce (transform.up * (100));
+			rb.AddForce (transform.right * -(100));
+			rb.velocity = new Vector2 (rb.velocity.x, rb.velocity.y);
 			// 5. Stun / Recovery Time
 			//charSpeed.bodyState = 0;
 		}
