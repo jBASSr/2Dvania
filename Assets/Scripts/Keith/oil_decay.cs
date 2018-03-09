77388481cb989bf3622e0b41ab97f22ca9eaa45d
Keith <keithharryman75@gmail.com>
2018-03-08 23:36:27 -0800

    added enemy prefabs, scripts

---
+++ Assets/Scripts/Keith/oil_decay.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class oil_decay : MonoBehaviour {
+
+	public GameObject oil_mess;
+	public float speed = 3.0f;
+	public float yRange = 3.0f;
+	public float xRange = 30.0f;
+	private Vector2 startPosition;
+	private CapsuleCollider2D rob_col, oildec_col;
+	private GameObject oilPrefab;
+	private GameObject robot;
+
+	// Use this for initialization
+	void Start () {
+		startPosition = transform.position;
+		robot = GameObject.Find ("Robot");
+		rob_col = robot.GetComponent<CapsuleCollider2D> ();
+		oildec_col = GetComponent<CapsuleCollider2D> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector2(transform.position.x + speed, startPosition.y + yRange * (Mathf.Sin(Time.time)));
+		if (Mathf.Abs(transform.position.x - startPosition.x) > xRange) {
+			speed *= -1.0f;
+		}
+
+		bool inx = (rob_col.bounds.min.x < oildec_col.bounds.max.x) && (rob_col.bounds.max.x > oildec_col.bounds.min.x);
+		bool iny = (rob_col.bounds.min.y < oildec_col.bounds.max.y) && (rob_col.bounds.max.y > oildec_col.bounds.min.y);
+		if (inx && iny){
+			Debug.Log ("OIL ON PLAYER!!!!");
+			if (oilPrefab == null) {
+				oilPrefab = (GameObject)Instantiate (
+					oil_mess,
+					transform.position,
+					transform.rotation);
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/Keith/tileParallax.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof(SpriteRenderer))]
+
+public class tileParallax : MonoBehaviour {
+
+	public int offsetX = 2;
+
+	public bool hasRightTile = false;
+	public bool hasLeftTile = false;
+	public bool reverseScale = false;
+
+	private float spriteWidth = 0f;
+	private Camera cam;
+	private Transform myTransform;
+
+	public float smoothing = 1f;   // Parallax smooth effect
+	private float parallaxScale = 0.0f;        // Proportion to move backgrounds based on movement
+
+	private Vector3 prevCamPos;    // Position of camera in previous frame
+	SpriteRenderer sr;
+
+
+	void Awake () {
+		cam = Camera.main;
+		myTransform = transform;
+	}
+
+	// Use this for initialization
+	void Start () {
+		sr = GetComponent<SpriteRenderer> ();
+		spriteWidth = sr.sprite.bounds.size.x;
+
+		prevCamPos = cam.transform.position;
+		// Iterate through backgrounds to assign parallaxScales
+		parallaxScale = myTransform.position.z*-1; // Must have *-1 for effect to work
+	}
+
+	// Update is called once per frame
+	void Update () {
+		float parallax = (prevCamPos.x - cam.transform.position.x) * parallaxScale;
+	     // Apply parallax
+		float myTransformTargetPosX = myTransform.position.x + parallax;
+	     // Target Position
+		Vector3 myTransformTargetPos = new Vector3 (myTransformTargetPosX, myTransform.position.y, myTransform.position.z);
+	     // Fade effect (between current and target position) using lerp
+		myTransform.position = Vector3.Lerp (myTransform.position, myTransformTargetPos, smoothing * Time.deltaTime);
+		prevCamPos = cam.transform.position;
+		if (hasLeftTile == false || hasRightTile == false) {
+			// Calculate what camera can see 
+			float camHorizontalExtend = cam.orthographicSize * Screen.width/Screen.height;
+			// Calculate where camera can see edge of a sprite
+			float edgeVisiblePosRight = (myTransform.position.x + spriteWidth/2) - camHorizontalExtend;
+			float edgeVisiblePosLeft = (myTransform.position.x - spriteWidth/2) + camHorizontalExtend;
+			// Test if edge of sprite is visble
+			if (cam.transform.position.x >= edgeVisiblePosRight - offsetX && hasRightTile == false) {
+				CreateTile(1);
+				hasRightTile = true;
+			} else if (cam.transform.position.x <= edgeVisiblePosLeft + offsetX && hasLeftTile == false) {
+				CreateTile(-1);
+				hasLeftTile = true;
+			}
+		}
+	}
+
+	void CreateTile (int side) {
+		Debug.Log ("Create Tile Called." + this.tag);
+		// Calculate position
+		Vector3 newPos;
+		if (side > 0) {
+			newPos = new Vector3 (sr.bounds.max.x + spriteWidth * side, myTransform.position.y, myTransform.position.z);
+		} else {
+			newPos = new Vector3 (sr.bounds.min.x + spriteWidth * side, myTransform.position.y, myTransform.position.z);
+		}
+		// Instantiate and assign new tile
+		Transform newTile = Instantiate (myTransform, newPos, myTransform.rotation) as Transform;
+
+		// If sprite is not tilable simply scale to -1
+		if (reverseScale == true) {
+			newTile.localScale = new Vector3 (newTile.localScale.x * -1, newTile.localScale.y, newTile.localScale.z);
+		}
+		// Assign to parent
+		if (side > 0) {
+			newTile.GetComponent<tileParallax> ().hasLeftTile = true;
+		} else {
+			newTile.GetComponent<tileParallax> ().hasRightTile = true;
+		}
+	}
+}
