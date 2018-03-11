f22db8550d8e63092651be2c4ef3bc9876794105
Keith <keithharryman75@gmail.com>
2018-03-11 14:37:04 -0700

    fixed cave scene

---
+++ Assets/key.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key : MonoBehaviour {
+	private SimpleMovement robot;
+
+	// Use this for initialization
+	void Start () {
+		robot = GameObject.Find ("Robot").GetComponent<SimpleMovement>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	void OnCollisionEnter2D(Collision2D coll)
+	{		
+		if (coll.gameObject.tag == "Player"){
+			Debug.Log("GOT THE KEY!");
+			Destroy (this.gameObject);
+		}
+    }
+}
