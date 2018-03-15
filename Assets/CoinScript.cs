121d19aa8ab54d4f764da3e7df99278d7ca338b1
blueberrykary <ksalinas3@csub.edu>
2018-03-15 04:38:03 -0700

    Added Coins and Score
    Added coins in cave scene

---
+++ Assets/CoinScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinScript : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        ScoreTextScript.coinAmount += 1;
+        Destroy(gameObject);
+
+    }
+}
