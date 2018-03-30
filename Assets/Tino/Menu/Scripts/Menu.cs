9b55067ddda25536921a6bdea7e023aef50220c7
Cristino Lamadrid <cristinocristino@gmail.com>
2018-03-30 15:32:01 -0700

    Fixed up Menu scene a little bit.

--- Assets/Tino/Menu/Scripts/Menu.cs
+++ Assets/Tino/Menu/Scripts/Menu.cs
@@ -12,7 +12,6 @@ namespace Tino
         private Sprite[] Strings;
         private GameObject[,] MainMenu = new GameObject[5, 2];
         private GameObject[,] OptionsMenu = new GameObject[3, 2];
-        private GameObject[,] NewGameMenu = new GameObject[2, 2];
 
         private int SelectedOption = 0;
         private bool FreshMenu = false;
@@ -50,9 +49,7 @@ namespace Tino
 
             if (this.SelectedOption == 0 && Input.GetKeyDown(KeyCode.Return))
             {
-                this.HideMenu(this.MainMenu);
-                this.ShowMenu(this.NewGameMenu);
-                this.Handler = this.NewGameMenuHandler;
+                UnityEngine.SceneManagement.SceneManager.LoadScene(this.NewGameScene);
             }
 
             if (this.SelectedOption == 1 && Input.GetKeyDown(KeyCode.Return))
@@ -102,40 +99,6 @@ namespace Tino
             }
         }
 
-        private void NewGameMenuHandler()
-        {
-            int previousSelection = this.SelectedOption;
-            if (Input.GetKeyDown(KeyCode.UpArrow)) { this.SelectedOption -= 1; }
-            else if (Input.GetKeyDown(KeyCode.DownArrow)) { this.SelectedOption += 1; }
-            this.SelectedOption = Mathf.Clamp(this.SelectedOption, 0, this.NewGameMenu.GetLength(0) - 1);
-
-            if (this.SelectedOption != previousSelection || this.FreshMenu)
-            {
-                this.FreshMenu = false;
-                this.UnboldMenuItem(this.NewGameMenu, previousSelection);
-                this.BoldMenuItem(this.NewGameMenu, this.SelectedOption);
-            }
-
-            if (Input.GetKeyDown(KeyCode.Return))
-            {
-                if (this.SelectedOption == 0)
-                {
-                    UnityEngine.SceneManagement.SceneManager.LoadScene(this.NewGameScene);
-                }
-                else if (this.SelectedOption == 1)
-                {
-                    UnityEngine.SceneManagement.SceneManager.LoadScene(this.NewGameScene);
-                }
-            }
-
-            if (Input.GetKeyDown(KeyCode.Escape))
-            {
-                this.HideMenu(this.NewGameMenu);
-                this.ShowMenu(this.MainMenu);
-                this.Handler = this.MainMenuHandler;
-            }
-        }
-
         private void CreateMenus()
         {
             this.MainMenu[0, 0] = this.CreateMenuItem(new Vector3(0, 200, 0), this.Strings[0]);
@@ -156,11 +119,6 @@ namespace Tino
             this.OptionsMenu[2, 0] = this.CreateMenuItem(new Vector3(0, 0, 0), this.Strings[8]);
             this.OptionsMenu[2, 1] = this.CreateMenuItem(new Vector3(0, 0, 0), this.Strings[17]);
             this.HideMenu(this.OptionsMenu);
-            this.NewGameMenu[0, 0] = this.CreateMenuItem(new Vector3(0, 200, 0), this.Strings[18]);
-            this.NewGameMenu[0, 1] = this.CreateMenuItem(new Vector3(0, 200, 0), this.Strings[21]);
-            this.NewGameMenu[1, 0] = this.CreateMenuItem(new Vector3(0, 100, 0), this.Strings[19]);
-            this.NewGameMenu[1, 1] = this.CreateMenuItem(new Vector3(0, 100, 0), this.Strings[22]);
-            this.HideMenu(this.NewGameMenu);
         }
 
         private void ShowMenu(GameObject[,] menu, int option = 0)
