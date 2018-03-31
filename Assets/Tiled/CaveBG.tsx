44bb9d2fd1a621f57ea867f3ba354ccf09d8d5d6
Keith <keithharryman75@gmail.com>
2018-03-31 13:01:48 -0700

    added bad_robot jump and walk sprites

---
+++ Assets/Tiled/CaveBG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="CaveBG" tilewidth="32" tileheight="32" tilecount="96" columns="16">
+ <image source="../../../workspace/2Dvania/Assets/Jose/Sprites/Environments/Cave/tile_cave_bg_fluo.png" width="512" height="192"/>
+</tileset>
