eaf2ba3211150b9b19ecb09598980fc27df9476a
Keith <keithharryman75@gmail.com>
2018-04-04 12:42:43 -0700

    added fireballs to boss robot

--- Assets/bad_robot.cs
+++ Assets/bad_robot.cs
@@ -6,19 +6,50 @@ public class bad_robot : MonoBehaviour {
 
 	public float xRange = 20.0f;
 	public bool is_right = true;
-	public float xStart = 0.0f;
-	public float xLast = 0.0f;
+
+	private float xStart = 0.0f;
+	private float xLast = 0.0f;
+
 	private Vector2 velocityNow;
+
 	public float speed = 3.0f;
 	public float jumpXFactor = 4.0f;
+
 	private Animator animator;
+
 	public float attackRange = 5.0f;
+
 	private GameObject robot;
 	private Rigidbody2D rb;
+
 	public float jumpForce = 20.0f;
+
 	private bool isGrounded;
+	private GameObject fireballLeft;
+	private GameObject fireballRight;
+
+	public GameObject fireballLeftEmitter;
+	public GameObject fireballRightEmitter;
+	public GameObject fireballLeftPrefab;
+	public GameObject fireballRightPrefab;
+
 	// Use this for initialization
+	public float fireRate = 1.0F;
+	public float fireball_speed = 5.0f;
+	private float lastFire = 0.0f;
+
+	private float startAttack = 0.0f;
+	private float attackTime = 3.0f;
+
+	private bool isAttacking = false;
+	private float yPos = 0.0f;
+	private float offsetTime = 0.0f;
+	public float hitCount = 10.0f;
+	private Color old_color;
+
+
 	void Start () {
+		old_color = gameObject.GetComponent<SpriteRenderer> ().color;
 		animator = GetComponent<Animator> ();
 		robot = GameObject.Find ("Robot");
 		isGrounded = true;
@@ -59,6 +90,7 @@ public class bad_robot : MonoBehaviour {
 			}
 			rb.AddForce (new Vector2 (0.0f, jumpForce));
 			isGrounded = false;
+			//isAttacking = true;
 		} else {
 			animator.SetBool ("is_jump", false);
 		}
@@ -71,6 +103,7 @@ public class bad_robot : MonoBehaviour {
 				animator.SetBool ("is_right", false);
 			}
 		}
+
 	}
 
 	void OnCollisionEnter2D(Collision2D coll)
@@ -78,6 +111,48 @@ public class bad_robot : MonoBehaviour {
 		if (coll.gameObject.tag == "Ground") {
 			Debug.Log("BAD ROBOT FELL TO GROUND!");
 			isGrounded = true;
+			//NOW FIRE!!!!:
+			if (Time.time > lastFire + fireRate) {
+				lastFire = Time.time;
+				Fire ();
+			}
 		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D c) {
+		if (c.tag == "Bullet") {
+			Debug.Log ("Bullet COLLIDED WITH BAD ROBOT!");
+			Destroy (c.gameObject);
+			hitCount -= 0.5f;
+			if (hitCount <= 0) {
+				Destroy (this.gameObject);
+			}
+
+			//animator.SetBool ("is_shocked", true);
+			StartCoroutine(hitRobot());
+			//animator.SetBool ("is_shocked", false);
+		}
+	}
+
+	IEnumerator hitRobot (){
+		gameObject.GetComponent<SpriteRenderer>().color=new Color(0, 0, 1, 1);
+		yield return new WaitForSeconds(0.2f);
+		gameObject.GetComponent<SpriteRenderer>().color=old_color;
+	}
+
+
+	void Fire(){
+		var fireballLeft = (GameObject)Instantiate (
+			fireballLeftPrefab,
+			fireballLeftEmitter.transform.position,
+			fireballLeftEmitter.transform.rotation);
+		fireballLeft.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -fireball_speed);
+		var fireballRight = (GameObject)Instantiate (
+			fireballRightPrefab,
+			fireballRightEmitter.transform.position,
+			fireballRightEmitter.transform.rotation);
+		fireballRight.GetComponent<Rigidbody2D> ().velocity = new Vector2 (0, -fireball_speed);
+
 	}
 }
---
+++ Assets/fireball.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class fireball : MonoBehaviour {
+
+	private Color old_color;
+	private GameObject robot;
+	// Use this for initialization
+	void Start () {
+		robot = GameObject.Find ("Robot");
+		old_color = robot.gameObject.GetComponent<SpriteRenderer> ().color;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D c)
+	{
+		if (c.tag == "Player") {
+			Destroy (this.gameObject, 0.2f);
+			//this.gameObject.GetComponent<SpriteRenderer>().color=new Color(1, 1, 0, 1);
+			this.gameObject.GetComponent<SpriteRenderer>().color=new Color(0, 0, 0, 1);
+			//StartCoroutine(hitPlayer());
+		}
+	}
+
+	IEnumerator hitPlayer (){		
+		Debug.Log ("HIT PLAYER CALLED!");
+		this.gameObject.GetComponent<SpriteRenderer>().color=new Color(0, 0, 0, 1);
+		yield return new WaitForSeconds(0.2f);
+		robot.gameObject.GetComponent<SpriteRenderer>().color=old_color;
+	}
+}
