9cc16c5685ea4863cf38986979237a65bd4503d6
Cristino <cristino.lamadrid@xceldata.com>
2018-04-17 20:17:58 -0700

    Added HPRefillDrop item that can be dropped by enemy. It doesnt need an entry in WorldState.cs Also added a MissilePickupDrop that does the same thing for missile ammo.

---
+++ Assets/Tino/Item/Scripts/HPRefillDrop.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Tino
+{
+    public class HPRefillDrop : MonoBehaviour
+    {
+        public int RefillAmount = 0;
+
+        void OnTriggerEnter2D(Collider2D c)
+        {
+            if (c.gameObject.tag != "Player")
+            {
+                return;
+            }
+            HealthSystem playerHealth = c.gameObject.GetComponent<HealthSystem>();
+            if (playerHealth == null) { return; }
+            if (playerHealth.RefillHealth(RefillAmount))
+            {
+                Destroy(this.gameObject);
+            }
+        }
+    }
+}
+
